b8a8a0db1992afcd543bd1cc798f17dab2b41177
arorasumit293 <arorasumit293@gmail.com>
2023-07-26 23:56:04 +0530

    added demo and calc

---
+++ Demo.java
@@ -0,0 +1,22 @@
+class calculator
+{
+    int a;
+
+     public int add()
+     {
+        System.out.println("in add");
+        return 0;
+     }
+
+}
+
+class Demo
+{
+    public static void main(String[] args)
+    {
+        calculator cal = new calculator(); 
+        cal.add();
+
+    }
+
+}
